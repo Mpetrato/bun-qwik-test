ea8cbd29f1ac1a3107a54c685b5728c946527b0e
matheus <m.rocha@vertex.vertexdigital.co>
2023-09-28 00:57:16 -0300

    teste analitics

--- src/root.tsx
+++ src/root.tsx
@@ -5,6 +5,7 @@ import {
   ServiceWorkerRegister,
 } from "@builder.io/qwik-city";
 import { RouterHead } from "./components/router-head/router-head";
+import { Analytics } from '@vercel/analytics/react';
 
 import "./global.css";
 
@@ -26,6 +27,7 @@ export default component$(() => {
       </head>
       <body lang="en">
         <RouterOutlet />
+        <Analytics />
       </body>
     </QwikCityProvider>
   );
